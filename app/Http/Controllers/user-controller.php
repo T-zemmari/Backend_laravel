4bab33b3be1d0555a99d0561b9d1b0cc7bc7bc7d
t.zemmari <tarik.zemmari@xabec.es>
2021-04-13 17:00:14 +0200

    added user controller

---
+++ app/Http/Controllers/user-controller.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+
+class userController extends Controller
+{
+    // Traer a Todos los usuarios: 
+
+
+               public function getAllUsers(){
+
+                 // Route::get('/',function()
+                 //    return view('users');
+                 // })
+
+               }
+
+    // Traer a un solo usuario: 
+
+                public function getOneUser(){
+
+
+                }
+
+
+    // Registrar a un usuario : 
+ 
+                public function registerOne(){
+       
+       
+                }
+
+
+    // Actualizar los datos de un usuario : 
+
+                public function  updateOne(){
+
+
+
+                }
+
+
+    // Borrar a un usuario de la base de datos : 
+
+
+                public function deleteOne(){
+
+
+
+                }
+
+
+}
