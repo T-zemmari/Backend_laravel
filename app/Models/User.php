5cd9e7f29c1f22be48e2f6ce64534837d75cf45e
t.zemmari <tarik.zemmari@xabec.es>
2021-04-14 15:12:11 +0200

    user model created

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,33 +11,17 @@ class User extends Authenticatable
 {
     use HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
+     protected $fillable = [
         'name',
         'email',
         'password',
+        'username'
     ];
+    public function posts () {
+        
+        return $this->hasMany('App\Models\Post','idusuario');
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
+    }
 
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+    
 }
