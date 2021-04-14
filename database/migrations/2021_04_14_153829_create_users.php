d4c6fa15086dd68f4bba9a086899c76af4353ec9
Daniel <danielcastillog92@gmail.com>
2021-04-14 19:17:29 +0200

    DB created. Added user migration

--- database/migrations/2021_04_14_153829_create_users.php
+++ database/migrations/2021_04_14_153829_create_users.php
@@ -15,6 +15,12 @@ class CreateUsers extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string('nickName');
+            $table->string('password');
+            $table->string('name');
+            $table->string('email');
+            $table->string('phone');
+            $table->string('adress');
             $table->timestamps();
         });
     }
